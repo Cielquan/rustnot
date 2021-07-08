3306f82b8e85d12db067a4120fbd32ed9a0fed47
Christian Riedel <cielquan@protonmail.com>
2021-07-08 06:46:06 +0200

    add main loop

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 extern crate winrt_notification;
 #[cfg(unix)]
 use notify_rust::{Notification, Timeout};
+use std::{thread, time};
 #[cfg(target_os = "windows")]
 use winrt_notification::{Duration, Sound, Toast};
 
@@ -11,22 +12,28 @@ enum CurrentStance {
 }
 
 fn main() {
-    let mut current_stance = CurrentStance::Sitting;
+    let mut current_stance = CurrentStance::Standing;
+    let mut waiting_time: u64;
 
-    match current_stance {
-        CurrentStance::Standing => {
-            send_notification("Sit Down!", "45");
-            current_stance = CurrentStance::Sitting;
+    loop {
+        match current_stance {
+            CurrentStance::Standing => {
+                current_stance = CurrentStance::Sitting;
+                waiting_time = 45;
+                send_notification("Sit Down!", waiting_time);
+            }
+            CurrentStance::Sitting => {
+                current_stance = CurrentStance::Standing;
+                waiting_time = 15;
+                send_notification("Stand Up!", waiting_time);
+            }
         }
-        CurrentStance::Sitting => {
-            send_notification("Stand Up!", "15");
-            current_stance = CurrentStance::Standing;
-        }
+        thread::sleep(time::Duration::from_secs(waiting_time));
     }
 }
 
 #[cfg(target_os = "windows")]
-fn send_notification(prompt: &str, next_toast: &str) {
+fn send_notification(prompt: &str, next_toast: u64) {
     Toast::new(Toast::POWERSHELL_APP_ID)
         .title(prompt)
         .text1("It's time to change your stance.")
