0338d236bc1d5f29eb3905c471911c027594b3d6
Christian Riedel <cielquan@protonmail.com>
2022-01-17 18:58:06 +0100

    add ui app (w/o timer impl)

---
+++ src/app.rs
@@ -0,0 +1,211 @@
+use std::borrow::BorrowMut;
+
+use iced::{
+    button, text_input, Align, Application, Button, Clipboard, Column, Command, Container, Element,
+    Length, Row, Text, TextInput,
+};
+
+use crate::{config, style};
+
+#[derive(Default)]
+pub struct App {
+    config: config::Config,
+    theme: style::Theme,
+    state: State,
+}
+
+struct State {
+    sit_time: text_input::State,
+    stand_time: text_input::State,
+    save_button: button::State,
+    config_saved: bool,
+    timer_button: button::State,
+    timer_running: bool,
+}
+
+impl Default for State {
+    fn default() -> Self {
+        Self {
+            sit_time: text_input::State::default(),
+            stand_time: text_input::State::default(),
+            save_button: button::State::default(),
+            config_saved: false,
+            timer_button: button::State::default(),
+            timer_running: false,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    SitTimeChanged(String),
+    StandTimeChanged(String),
+    ConfigLoaded(Result<config::Config, config::ConfigFileError>),
+    SaveConfig,
+    ConfigSaved(Result<(), config::ConfigFileError>),
+    StartTimer,
+    StopTimer,
+}
+
+impl<'a> Application for App {
+    type Executor = iced::executor::Default;
+    type Message = Message;
+    type Flags = ();
+
+    fn new(_flags: ()) -> (Self, Command<Message>) {
+        (
+            Self::default(),
+            Command::perform(config::Config::load_from_file(), Message::ConfigLoaded),
+        )
+    }
+
+    fn title(&self) -> String {
+        String::from("rustnot")
+    }
+
+    fn update(&mut self, message: Message, _: &mut Clipboard) -> Command<Message> {
+        match message {
+            Message::SitTimeChanged(input) => {
+                if let Ok(dur) = input.parse::<u32>() {
+                    self.config.sit_time = dur;
+                    self.state.config_saved = false;
+                }
+            }
+            Message::StandTimeChanged(input) => {
+                if let Ok(dur) = input.parse::<u32>() {
+                    self.config.stand_time = dur;
+                    self.state.config_saved = false;
+                }
+            }
+            Message::ConfigLoaded(res) => {
+                if let Ok(new_conf) = res {
+                    self.config = new_conf;
+                    self.state.config_saved = true;
+                }
+            }
+            Message::SaveConfig => {
+                return Command::perform(
+                    config::Config::save_to_file(self.config.clone()),
+                    Message::ConfigSaved,
+                );
+            }
+            Message::ConfigSaved(res) => {
+                if res.is_ok() {
+                    self.state.config_saved = true;
+                } else {
+                    self.state.config_saved = false;
+                }
+            }
+            Message::StartTimer => {
+                self.state.timer_running = true;
+            }
+            Message::StopTimer => {
+                self.state.timer_running = false;
+            }
+        }
+        Command::none()
+    }
+
+    fn view(&mut self) -> Element<Message> {
+        const LABLE_WIDTH: u16 = 170;
+        const TEXT_SIZE: u16 = 30;
+        const PADDING: u16 = 10;
+
+        let sit_time = Row::new()
+            .padding(PADDING)
+            .push(
+                Text::new("Sit Time:")
+                    .width(Length::Units(LABLE_WIDTH))
+                    .size(TEXT_SIZE),
+            )
+            .push(
+                TextInput::new(
+                    self.state.sit_time.borrow_mut(),
+                    "Sit time",
+                    &self.config.sit_time.to_string()[..],
+                    Message::SitTimeChanged,
+                )
+                .size(TEXT_SIZE)
+                .style(self.theme),
+            );
+
+        let stand_time = Row::new()
+            .padding(PADDING)
+            .push(
+                Text::new("Stand Time:")
+                    .width(Length::Units(LABLE_WIDTH))
+                    .size(TEXT_SIZE),
+            )
+            .push(
+                TextInput::new(
+                    self.state.stand_time.borrow_mut(),
+                    "Stand time",
+                    &self.config.stand_time.to_string()[..],
+                    Message::StandTimeChanged,
+                )
+                .size(TEXT_SIZE)
+                .style(self.theme),
+            );
+
+        let save_state_text;
+        if self.state.config_saved {
+            save_state_text = "Config saved";
+        } else {
+            save_state_text = "Unsaved config";
+        }
+
+        let save_btn = Column::new()
+            .padding(PADDING * 2)
+            .align_items(Align::Center)
+            .push(
+                Button::new(
+                    &mut self.state.save_button,
+                    Text::new("Save config to file"),
+                )
+                .padding(PADDING)
+                .style(self.theme)
+                .on_press(Message::SaveConfig),
+            )
+            .push(Text::new(save_state_text).size(TEXT_SIZE / 2));
+
+        let btn_text;
+        let btn_on_press;
+        let timer_state_text;
+        if self.state.timer_running {
+            btn_text = "Stop Timer";
+            btn_on_press = Message::StopTimer;
+            timer_state_text = "Running ...";
+        } else {
+            btn_text = "Start Timer";
+            btn_on_press = Message::StartTimer;
+            timer_state_text = "Stopped";
+        }
+
+        let timer_btn = Column::new()
+            .padding(PADDING * 2)
+            .align_items(Align::Center)
+            .push(
+                Button::new(&mut self.state.timer_button, Text::new(btn_text))
+                    .padding(PADDING)
+                    .style(self.theme)
+                    .on_press(btn_on_press),
+            )
+            .push(Text::new(timer_state_text).size(TEXT_SIZE / 2));
+
+        let content = Column::new()
+            .padding(PADDING)
+            .align_items(Align::Center)
+            .push(sit_time)
+            .push(stand_time)
+            .push(save_btn)
+            .push(timer_btn);
+
+        Container::new(content)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .center_x()
+            .center_y()
+            .style(self.theme)
+            .into()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,35 +3,20 @@
 #[macro_use]
 extern crate serde_derive;
 
+mod app;
 mod config;
 mod notification;
+mod style;
 
-use std::{thread, time};
+use iced::{window, Application, Settings};
 
-enum CurrentStance {
-    Standing,
-    Sitting,
-}
-
-fn main() {
-    let mut current_stance = CurrentStance::Standing;
-    let mut waiting_time: u32;
-    config::load_config().unwrap();
-    config::save_config().unwrap();
-
-    loop {
-        match current_stance {
-            CurrentStance::Standing => {
-                current_stance = CurrentStance::Sitting;
-                waiting_time = (*config::CONFIG.lock()).sit_time;
-                notification::send_notification("Sit Down!", waiting_time);
-            }
-            CurrentStance::Sitting => {
-                current_stance = CurrentStance::Standing;
-                waiting_time = (*config::CONFIG.lock()).stand_time;
-                notification::send_notification("Stand Up!", waiting_time);
-            }
-        }
-        thread::sleep(time::Duration::from_secs(waiting_time as u64 * 60));
-    }
+pub fn main() -> iced::Result {
+    app::App::run(Settings {
+        // antialiasing: true,
+        window: window::Settings {
+            size: (350, 350),
+            ..window::Settings::default()
+        },
+        ..Settings::default()
+    })
 }
