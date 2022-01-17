06c2e30370b1a6fe2fc956a664fd9a4e2081bacc
Christian Riedel <cielquan@protonmail.com>
2022-01-17 12:50:37 +0100

    add notification mod

---
+++ src/notification.rs
@@ -0,0 +1,30 @@
+#[cfg(unix)]
+use notify_rust::{Notification, Timeout};
+#[cfg(target_os = "windows")]
+use winrt_notification::{Duration, Sound, Toast};
+
+#[cfg(unix)]
+pub fn send_notification(prompt: &str, next_toast_in: u64) {
+    Notification::new()
+        .summary(prompt)
+        .body(&format!(
+            "It's time to change your stance.\nNext reminder in: {} min.",
+            next_toast_in
+        ))
+        .sound_name("dialog-information")
+        .timeout(Timeout::Never)
+        .show()
+        .expect("unable to toast");
+}
+
+#[cfg(target_os = "windows")]
+pub fn send_notification(prompt: &str, next_toast_in: u64) {
+    Toast::new(Toast::POWERSHELL_APP_ID)
+        .title(prompt)
+        .text1("It's time to change your stance.")
+        .text2(&format!("Next reminder in: {} min.", next_toast_in))
+        .sound(Some(Sound::Default))
+        .duration(Duration::Long)
+        .show()
+        .expect("unable to toast");
+}
