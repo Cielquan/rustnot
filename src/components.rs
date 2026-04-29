3871ddf7588d7a9329443bcc1f6164b9ebc8daab
Christian Riedel <cielquan@protonmail.com>
2026-04-29 19:54:52 +0200

    add optional size param to icon component

--- src/components.rs
+++ src/components.rs
@@ -29,20 +29,20 @@ where
     .into()
 }
 
-pub fn icon<'a>(icon_file_path: &str) -> Svg<'a, iced::Theme> {
+pub fn icon<'a>(icon_file_path: &str, size: Option<u32>) -> Svg<'a, iced::Theme> {
     svg(icon_file_path)
         .content_fit(iced::ContentFit::Contain)
         .style(|theme: &iced::Theme, _style| svg::Style {
             color: Some(theme.palette().text),
         })
-        .height(25)
-        .width(25)
+        .height(size.unwrap_or(25))
+        .width(size.unwrap_or(25))
 }
 
 pub fn icon_button<'a, Message>(
     icon_file_path: &str,
 ) -> Button<'a, Message, iced::Theme, iced::Renderer> {
-    button(icon(icon_file_path))
+    button(icon(icon_file_path, None))
         .width(iced::Length::Shrink)
         .height(iced::Length::Shrink)
         .padding(7)
