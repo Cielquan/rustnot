ec33d7f8786ce68ad33d877d639e5c2976e40e9f
Christian Riedel <cielquan@protonmail.com>
2026-04-29 19:32:35 +0200

    add icon component

--- src/components.rs
+++ src/components.rs
@@ -29,18 +29,22 @@ where
     .into()
 }
 
+pub fn icon<'a>(icon_file_path: &str) -> Svg<'a, iced::Theme> {
+    svg(icon_file_path)
+        .content_fit(iced::ContentFit::Contain)
+        .style(|theme: &iced::Theme, _style| svg::Style {
+            color: Some(theme.palette().text),
+        })
+        .height(25)
+        .width(25)
+}
+
 pub fn icon_button<'a, Message>(
     icon_file_path: &str,
 ) -> Button<'a, Message, iced::Theme, iced::Renderer> {
-    button(
-        svg(icon_file_path)
-            .content_fit(iced::ContentFit::Contain)
-            .style(|theme: &iced::Theme, _style| svg::Style {
-                color: Some(theme.palette().text),
-            }),
-    )
-    .width(iced::Length::Shrink)
-    .height(iced::Length::Shrink)
-    .padding(7)
-    .style(button::background)
+    button(icon(icon_file_path))
+        .width(iced::Length::Shrink)
+        .height(iced::Length::Shrink)
+        .padding(7)
+        .style(button::background)
 }
